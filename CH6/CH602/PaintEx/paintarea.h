cb2b37413608b357b989f6aa48a94368e6f4ca78
mrhao <mrhao@jiawel>
2018-04-11 11:50:31 +0800

    add CH6

---
+++ CH6/CH602/PaintEx/paintarea.h
@@ -0,0 +1,45 @@
+#ifndef PAINTAREA_H
+#define PAINTAREA_H
+
+#include <QWidget>
+#include <QPen>
+#include <QBrush>
+
+class PaintArea : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PaintArea(QWidget *parent = nullptr);
+
+    enum Shape{
+        Line,
+        Rectangle,
+        RoundRect,
+        Ellipse,
+        Polygon,
+        Polyline,
+        Points,
+        Arc,
+        Path,
+        Text,
+        Pixmap
+    };
+
+void setShape(Shape);
+void setPen(QPen);
+void setBrush(QBrush);
+void setFillRule(Qt::FillRule);
+void paintEvent(QPaintEvent*);
+
+private:
+    Shape shape;
+    QPen pen;
+    QBrush brush;
+    Qt::FillRule fillRule;
+
+signals:
+
+public slots:
+};
+
+#endif // PAINTAREA_H
